922d6531ee91927d1a60abc1a7ca17ae05b0151f
GarretR <garret.rueckert.guitar@gmail.com>
2018-12-01 09:25:41 -0700

    Nearly done, perhaps display the name of the song

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,17 +15,20 @@ $(document).ready(function() {
   var startBtn = $("#start");
   var numCorrect = 0;
   var numIncorrect = 0;
+  var numMissed = 0;
   var totalQs = 6;
 
+  //Text for the quuestions
   var questionTextArray = [
     '"Just a small town _______"',
     '"_______ has got it going on"',
     '"The boys are back _______"',
     '"In the days of my youth, I was told what it means _______"',
     '"You show us everything you got,\nYou keep on dancing and the room gets hot,\nYou drive us wild _______!"',
-    '"For those about to rock, _______!\n'
+    '"For those about to rock, _______!"'
   ];
 
+  //Hard coded answers, comments reflect the correct option
   var answers = [
     ["girl", "boy", "world", "man"], //0
     ["Stacy's bomb", "Stacy's mom", "sugar sugar", "Mickey"], //1
@@ -40,6 +43,15 @@ $(document).ready(function() {
     ["it's a long ways", "never stop", "fire away", "we salute you"] //3
   ];
 
+  var imgLinks = [
+    "assets/images/escape.png",
+    "assets/images/stacy.png",
+    "assets/images/jailbreak.jpg",
+    "assets/images/zeppelin.jpg",
+    "assets/images/kiss.jpg",
+    "assets/images/ACDC.jpg"
+  ];
+
   var correctAns = [0, 1, 0, 3, 2, 3];
 
   //four list items per question
@@ -47,6 +59,12 @@ $(document).ready(function() {
   var ans2 = $("<li>");
   var ans3 = $("<li>");
   var ans4 = $("<li>");
+
+  //splashscreen elements
+  var imgTag = $("<img>");
+  imgTag.addClass("splash img-fluid rounded");
+  var result = $("<div>");
+  result.addClass("splash");
 
   //clock variables
   var clockrunning = false;
@@ -56,8 +74,12 @@ $(document).ready(function() {
   var clock = {
     time: 10,
     questionNumber: 0,
+    splashTime: 5,
 
+    //Move to next question
     next: function() {
+      $(".splash").hide();
+      $(".io").show();
       clock.time = 10;
       if (clock.questionNumber < totalQs - 1) {
         clock.questionNumber++;
@@ -67,11 +89,13 @@ $(document).ready(function() {
       }
     },
 
+    //Set the question and update the timer back to 10
     updateDisplays() {
       populateQ(clock.questionNumber);
       $("#timer").text(clock.time);
     },
 
+    //Get clock running
     start: function() {
       if (!clockrunning) {
         clockrunning = true;
@@ -79,17 +103,45 @@ $(document).ready(function() {
       }
     },
 
+    //stop the clock
     stop: function() {
       clearInterval(intervalID);
       clockrunning = false;
     },
 
+    //Clock ticks down from 10 to 0
     count: function() {
       clock.time--;
       $("#timer").text(clock.time);
-      if (clock.time === 0) clock.next();
+      if (clock.time === 0) {
+        numMissed++;
+        splashScreen(clock.questionNumber, false);
+        setTimeout(function() {
+          clock.start();
+          clock.next();
+        }, 5000);
+      }
     }
   };
+
+  /**
+   * Displays a splash screen with appropriate image
+   * (per question) for a short time
+   * @param qNum The question number
+   *
+   */
+  function splashScreen(qNum, correct) {
+    clock.stop();
+    $(".io").hide();
+    $(".splash").show();
+    //clock.stop();
+
+    var txt = correct ? "Correct!" : "Incorrect :(";
+    result.html("<h2>"+txt+"</h2>");
+    imgTag.attr("src", imgLinks[qNum]);
+    $("#message").append(result);
+    $("#splashImg").append(imgTag);
+  }
 
   function populateQ(qNum) {
     var qText = questionTextArray[qNum];
@@ -105,16 +157,32 @@ $(document).ready(function() {
     $(answerList).append(ans4);
   }
 
+  /**
+   * Checks the selected answer vs what the correct answer would be per question
+   * @param selection The option selected
+   *
+   */
   function evaluate(selection) {
     if (selection == correctAns[clock.questionNumber]) {
       numCorrect++;
-      clock.next();
+      splashScreen(clock.questionNumber, true);
+      setTimeout(function() {
+        clock.start();
+        clock.next();
+      }, 5000);
     } else {
       numIncorrect++;
-      clock.next();
+      splashScreen(clock.questionNumber, false);
+      setTimeout(function() {
+        clock.start();
+        clock.next();
+      }, 5000);
     }
   }
 
+  /**
+   * Stops the clock, resets the game. Called on start/restart button function
+   */
   function endGame() {
     clock.stop();
     $(question).text("Game over! Here's how you did!");
@@ -125,35 +193,32 @@ $(document).ready(function() {
         Math.floor((numCorrect * 100) / totalQs) +
         "%!"
     );
-    $(ans4).text("");
+    $(ans4).text("You timed out on " + numMissed + " questions");
     $(answerList).append(startBtn);
     startBtn.text("Click to restart!");
     startBtn.show();
     numCorrect = 0;
     numIncorrect = 0;
     clock.questionNumber = 0;
-
   }
 
   $(ans1).on("click", function() {
-    if(clockrunning) evaluate(0);
+    if (clockrunning) evaluate(0);
   });
   $(ans2).on("click", function() {
-    if(clockrunning) evaluate(1);
+    if (clockrunning) evaluate(1);
   });
   $(ans3).on("click", function() {
-    if(clockrunning) evaluate(2);
+    if (clockrunning) evaluate(2);
   });
   $(ans4).on("click", function() {
-    if(clockrunning) evaluate(3);
+    if (clockrunning) evaluate(3);
   });
 
-  $(startBtn).on("click", function(){
+  $(startBtn).on("click", function() {
     $(startBtn).hide();
     $(".io").show();
     clock.start();
     clock.updateDisplays();
-  })
-
-  
+  });
 });
